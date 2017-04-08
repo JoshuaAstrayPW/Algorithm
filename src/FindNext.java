3a951f557040b5db9b099c821fc1b85515bd5948
JoshuaAstrayPW <xu_fang2016@hotmail.com>
2017-04-08 21:05:08 +0800

    update

--- src/FindNext.java
+++ src/FindNext.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * 请设计一个算法，寻找二叉树中指定结点的下一个结点（即中序遍历的后继）。
  给定树的根结点指针TreeNode* root和结点的值int p，
@@ -5,21 +7,18 @@
  保证结点的值大于等于零小于等于100000且没有重复值，若不存在后继返回-1。
  */
 public class FindNext {
-    public int findSucc(TreeNode root, int p) {
-       boolean f = false;
-       return find(root, p, f);
+    private TreeNode pre = new TreeNode(-1);
+        public int findSucc(TreeNode root, int p) {
+           if(root == null)
+               return  -1;
+           int left = findSucc(root.left, p);
+           if(left == -1){
+               if(pre.val == p)
+                   return root.val;
+               pre = root;
+               return findSucc(root.right, p);
+           }
+           return left;//如果找不到则返回上一层的根节点，方向为 左→根
+        }
 
-    }
-    public int find(TreeNode root, int p, boolean f){
-        if(root == null)
-            return -1;
-        int left = find(root.left, p, f);
-        if(left != -1)
-            return left;
-        if(f == true)
-            return root.val;
-        if(root.val == p)
-            f = true;
-        return find(root.right, p, f);
-    }
 }
---
+++ src/LCA.java
@@ -0,0 +1,16 @@
+/**
+ * Created by fangxu on 2017/4/8.
+ * 满二叉树的最近公共祖先
+ */
+public class LCA {
+    public int getLCA(int a, int b) {
+       while (a != b){
+           if(a > b)
+               a /= 2;
+           else
+               b /= 2;
+
+       }
+       return a;
+    }
+}
